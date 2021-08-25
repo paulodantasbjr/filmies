3db40885d3bd27f6f53a90bae6cffabbd73af71b
Paulo Dantas <paulo.dantas.solutic@petrobras.com.br>
2021-08-25 09:54:20 -0300

    product

---
+++ admin/src/pages/products/create.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Flex,
+  Divider,
+  Heading,
+  SimpleGrid,
+  VStack,
+  HStack,
+  Button,
+  Checkbox,
+} from '@chakra-ui/react'
+
+import { Input } from '../../components/Form/Input'
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} p={['6', '8']} bg='gray.200'>
+          <Heading size='lg' fontWeight='normal'>
+            Criar Produto
+          </Heading>
+
+          <Divider my='6' borderColor='gray.400' />
+
+          <VStack spacing='8'>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='title' label='Nome' type='text' />
+              <Input name='genre' label='Genero' type='text' />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='des' label='Descricao' type='text' />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='img' label='Imagem' type='file' />
+              <Input name='imgTitle' label='Imagem do Titulo' type='file' />
+              <Input name='thumbnail' label='Thumbnail' type='file' />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='trailer' label='Trailer' type='file' />
+              <Input name='video' label='Video' type='file' />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
+              <Input name='year' label='Ano do lancamento' type='number' />
+              <Input name='limit' label='Faixa etária' type='number' />
+              <Checkbox colorScheme='purple' borderColor='gray.500'>
+                Serie?
+              </Checkbox>
+            </SimpleGrid>
+          </VStack>
+          <Flex mt='8' justify='flex-end'>
+            <HStack spacing='4'>
+              <Button colorScheme='blackAlpha'>Cancelar</Button>
+              <Button colorScheme='purple'>Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
---
+++ admin/src/pages/products/index.tsx
@@ -0,0 +1,107 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Checkbox,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Tfoot,
+  Icon,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+import { RiEditFill, RiAddFill } from 'react-icons/ri'
+
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+import { Pagination } from '../../components/Pagination'
+
+export default function ProductList() {
+  const isMobileVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+  return (
+    <Box>
+      <Header />
+      <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} p={['2', '8']} bg='gray.200'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal' mr={['4', '0']}>
+              Produtos
+            </Heading>
+            <Button
+              as='a'
+              size='sm'
+              colorScheme='purple'
+              leftIcon={<Icon as={RiAddFill} />}
+            >
+              {isMobileVersion ? 'Criar novo' : ''}
+            </Button>
+          </Flex>
+
+          <Table variant='simple'>
+            <Thead>
+              <Tr>
+                {isMobileVersion && (
+                  <Th px='6'>
+                    <Checkbox colorScheme='purple' borderColor='gray.500' />
+                  </Th>
+                )}
+                <Th>Produtos</Th>
+                {isMobileVersion && <Th>Tipo</Th>}
+                {isMobileVersion && <Th>Data de cadastro</Th>}
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                {isMobileVersion && (
+                  <Td px='6'>
+                    <Checkbox colorScheme='purple' borderColor='gray.500' />
+                  </Td>
+                )}
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>Matrix</Text>
+                    <Text fontSize='sm' color='gray.400'>
+                      14 ‧ 1999 ‧ Ficção científica/Ação
+                    </Text>
+                  </Box>
+                </Td>
+                {isMobileVersion && <Td>Filme</Td>}
+
+                {isMobileVersion && <Td>24 de Agosto, 2021</Td>}
+                <Td>
+                  <Button
+                    as='a'
+                    size='sm'
+                    colorScheme='purple'
+                    leftIcon={<Icon as={RiEditFill} />}
+                  >
+                    {isMobileVersion ? 'Editar' : ''}
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th px={['4', '4', '6']}>Total</Th>
+                <Th>1</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
